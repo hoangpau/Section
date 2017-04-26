e5f3e10c9ed3d5c4461bb589a34c04c224d2e950
HienBuiDuy <buiduyhien352911@gmail.com>
2017-04-26 10:48:51 +0700

    Init Commit

---
+++ Section/Section/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Section
+//
+//  Created by Hoang on 4/26/17.
+//  Copyright © 2017 Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
